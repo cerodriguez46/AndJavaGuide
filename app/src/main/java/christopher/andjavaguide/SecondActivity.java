e705444efb1f42ca830d918952334cec4b715f84
Chris Rodriguez <cerdevelopers@gmail.com>
2018-11-30 00:23:43 -0600

    UPDATE: Obtained data passed from MainActivity into SecondActivity

--- app/src/main/java/christopher/andjavaguide/SecondActivity.java
+++ app/src/main/java/christopher/andjavaguide/SecondActivity.java
@@ -1,8 +1,21 @@
 package christopher.andjavaguide;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("CUSTOMESSAGE");
+        System.out.println(message);
+
+
+    }
 
 
 }
